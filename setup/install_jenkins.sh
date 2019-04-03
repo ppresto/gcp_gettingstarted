1452d3a7c08a99088de0f3680756ffb219b88cbe
i838855 <patrick.presto@sap.com>
2019-04-03 12:39:21 -0700

    fixed missing quote

--- setup/install_jenkins.sh
+++ setup/install_jenkins.sh
@@ -29,13 +29,14 @@ if [[ ${status} == "Running" ]]; then
   export POD_NAME=$(kubectl get pods -l "component=cd-jenkins-master" -o jsonpath="{.items[0].metadata.name}")
   kubectl get svc
   echo "Login as Admin with : $(kubectl get secret cd-jenkins -o jsonpath="{.data.jenkins-admin-password}" | base64 --decode)"
-  echo "Jenkins is running at http://localhost:8081 (only accessible from within the container)
+
+  #Jenkins is running at http://localhost:8081 (only accessible from within the container)
   kubectl port-forward $POD_NAME 8081:8080 >/dev/null &
 else
   echo "Status: ${status}"
   echo "No Pod available, Not setting port-forwarding"
 fi
-echo "kubectl doesnt support 0.0.0.0 yet so http://localhost:8081 only works within container"
-echo "Enable port-forward for nic 0.0.0.0 using socat for routing fix...  "
-echo "Access Jenkins at http://localhost:8080 "
+
+#kubectl doesnt support 0.0.0.0 yet so http://localhost:8081 only works within container
+echo "Enable port-forward with socat.  Access Jenkins at http://localhost:8080"
 socat tcp-listen:8080,fork tcp:127.0.0.1:8081 &
